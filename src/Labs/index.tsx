41325e2d7acd219a71a9a2c0400c1825ca8aed0a
alemirin <138611402+alemirin@users.noreply.github.com>
2024-09-19 17:40:02 -0400

    added section to landing page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,7 +8,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
-      <h2>Alejandro Miranda</h2>
+      <h2>Alejandro Miranda | CS5610_Fa_2024_Online</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
