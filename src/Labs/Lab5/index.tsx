ef745761c8e150724676c84182a5b9e0b7e96d68
alemirin <138611402+alemirin@users.noreply.github.com>
2024-11-13 17:02:23 -0400

    added link to lab5 node server

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,10 @@
+export default function Lab5() {
+  return (
+    <div>
+      <h1>Lab 5</h1>
+      <a href="http://localhost:4000/lab5">
+        <h2>Go to Lab 5</h2>
+      </a>
+    </div>
+  );
+}
