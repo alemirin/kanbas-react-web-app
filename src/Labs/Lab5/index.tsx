dbf5c37851c4d14b9bf73546e02aed62709baee7
alemirin <138611402+alemirin@users.noreply.github.com>
2024-11-14 15:11:19 -0400

    modified UI for Lab5 and added env variables

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,10 +1,15 @@
+const REMOTE_SERVER =
+  process.env.REACT_APP_REMOTE_SERVER || "http://localhost:4000";
 export default function Lab5() {
   return (
     <div>
-      <h1>Lab 5</h1>
-      <a href="http://localhost:4000/lab5">
-        <h2>Go to Lab 5</h2>
+      <br />
+      <h1 className="text-center">Lab 5</h1>
+      <a href={`${REMOTE_SERVER}/lab5`}>
+        <button className="btn btn-outline-primary w-100">Go to Lab 5</button>
       </a>
+      <hr />
+      Enrionment Variable: {REMOTE_SERVER}
     </div>
   );
 }
