3442a11fb2480dab3e4a1b28bf890a2f11aa87bc
alemirin <138611402+alemirin@users.noreply.github.com>
2024-11-22 18:54:49 -0400

    added up t finding modules for course using server

---
+++ src/Kanbas/Courses/client.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+const REMOTE_SERVER =
+  process.env.REACT_APP_REMOTE_SERVER || "http://localhost:4000";
+const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+export const fetchAllCourses = async () => {
+  const { data } = await axios.get(COURSES_API);
+  return data;
+};
+
+export const deleteCourse = async (id: string) => {
+  const { data } = await axios.delete(`${COURSES_API}/${id}`);
+  return data;
+};
+
+export const updateCourse = async (course: any) => {
+  const response = await axios.put(`${COURSES_API}/${course._id}`, course);
+  return response.data;
+};
+
+export const findModulesForCourse = async (courseId: string) => {
+  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
+};
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,14 +6,32 @@ import Session from "./Account/Session";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
-import * as db from "./Database";
-import { useState } from "react";
+import * as userClient from "./Account/client";
+import * as courseClient from "./Courses/client";
+import { useEffect, useState } from "react";
 import ProtectedRoute from "./Account/ProtectedRoute";
+import { useSelector } from "react-redux";
 
 export default function Kanbas() {
-  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [courses, setCourses] = useState<any[]>([]);
+
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
+  const fetchCourses = async () => {
+    try {
+      const courses = await userClient.findAllCourses();
+      setCourses(courses);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCourses();
+  }, [currentUser]);
+
   const [course, setCourse] = useState<any>({
-    _id: "0",
+    _id: "RS100",
     name: "New Course",
     number: "New Number",
     startDate: "2023-09-10",
@@ -21,15 +39,18 @@ export default function Kanbas() {
     image: "/images/reactjs.jpg",
     description: "New Description",
   });
-  const addNewCourse = () => {
-    const newCourse = { ...course, _id: new Date().getTime().toString() };
-    setCourses([...courses, { ...course, ...newCourse }]);
+
+  const addNewCourse = async () => {
+    const newCourse = await userClient.createCourse(course);
+    setCourses([...courses, { ...course, newCourse }]);
   };
-  const deleteCourse = (courseId: string) => {
+  const deleteCourse = async (courseId: string) => {
+    const status = await courseClient.deleteCourse(courseId);
     setCourses(courses.filter((course) => course._id !== courseId));
   };
 
-  const updateCourse = () => {
+  const updateCourse = async () => {
+    await courseClient.updateCourse(course);
     setCourses(
       courses.map((c) => {
         if (c._id === course._id) {
