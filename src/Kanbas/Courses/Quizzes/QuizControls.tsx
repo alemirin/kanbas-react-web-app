ab8e6782047d94c1b171b8dfb68759aa1506047b
alemirin <138611402+alemirin@users.noreply.github.com>
2024-12-11 17:11:26 -0400

    edited quiz control buttons

--- src/Kanbas/Courses/Quizzes/QuizControls.tsx
+++ src/Kanbas/Courses/Quizzes/QuizControls.tsx
@@ -1,6 +1,7 @@
 import { FaPlus } from "react-icons/fa6";
 import FacultyRoute from "../../Account/FacultyRoute";
 import { useNavigate, useParams } from "react-router";
+import { IoEllipsisVertical } from "react-icons/io5";
 
 export default function QuizControls({
   filterQuizzes,
@@ -12,24 +13,36 @@ export default function QuizControls({
   return (
     <div
       id="wd-assignment-controls"
-      className="text-nowrap d-flex flex-row-reverse gap-2"
+      className="d-flex align-items-center flex-row-reverse justify-content-between"
     >
-      <FacultyRoute>
-        <button
-          id="wd-add-assignment-btn"
-          className="btn btn-lg btn-danger me-1 float-end"
-          onClick={() => navigate(`/Kanbas/Courses/${cid}/Quizzes/new`)}
-        >
-          <FaPlus
-            className="position-relative me-2"
-            style={{ bottom: "1px" }}
-          />
-          Quiz
-        </button>
-      </FacultyRoute>
+      <div className="text-nowrap d-flex flex-row-reverse">
+        <FacultyRoute>
+          <button
+            id="wd-quiz-controls-btn"
+            className="btn btn-lg me-1 float-end"
+            style={{
+              border: "1px solid black",
+              backgroundColor: "#ccc",
+            }}
+          >
+            <IoEllipsisVertical className="fs-4 hoverable-icon" />
+          </button>
+          <button
+            id="wd-add-assignment-btn"
+            className="btn btn-lg btn-danger me-1 float-end"
+            onClick={() => navigate(`/Kanbas/Courses/${cid}/Quizzes/new`)}
+          >
+            <FaPlus
+              className="position-relative me-2"
+              style={{ bottom: "1px" }}
+            />
+            Quiz
+          </button>
+        </FacultyRoute>
+      </div>
       <input
         id="wd-search-assignment"
-        className="search-input rounded"
+        className="search-input form-control w-25 rounded ms-5 mt-2"
         type="search"
         placeholder="&#128270; Search..."
         onChange={(e) => filterQuizzes(e.target.value)}
